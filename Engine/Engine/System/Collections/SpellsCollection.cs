b76664be3e596906a09093b3d0e3634fd8860571
Nair Sark <serkorit@gmail.com>
2017-11-07 18:16:00 +0500

    Spells Collection added

---
+++ Engine/Engine/System/Collections/SpellsCollection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Magick;
+
+namespace Engine.System.Collections
+{
+    public class SpellsCollection
+    {
+        public Spell Spell { get; set; }
+
+        public SpellsCollection(Spell spell)
+        {
+            Spell = spell;
+        }
+    }
+}
