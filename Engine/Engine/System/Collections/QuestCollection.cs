3be81eaac799a10ae6a18e2377377627e41c4ad8
Nair Sark <serkorit@gmail.com>
2017-11-07 18:10:51 +0500

    Quests collection

---
+++ Engine/Engine/System/Collections/QuestCollection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.World;
+
+namespace Engine.System.Collections
+{
+    public class QuestCollection
+    {
+        public Quest Quest { get; set; }
+        public bool IsComplete { get; set; }
+
+        public QuestCollection(Quest quest)
+        {
+            Quest = quest;
+            IsComplete = false;
+        }
+    }
+}
---
+++ Engine/Engine/System/Collections/QuestConditionsCollection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Items;
+
+namespace Engine.System.Collections
+{
+    class QuestConditionsCollection
+    {
+        public List<Item> Items { get; set; }
+        public List<int> Quanity { get; set; }
+
+        public QuestConditionsCollection(List<Item> items, List<int> quanity)
+        {
+            Items = items;
+            Quanity = quanity;
+        }
+    }
+}
