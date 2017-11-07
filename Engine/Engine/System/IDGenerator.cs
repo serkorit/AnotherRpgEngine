eb784a31ee998b2cf4087185e94de38acafa9a2c
Nair Sark <serkorit@gmail.com>
2017-11-07 18:15:06 +0500

    Random number generator added

---
+++ Engine/Engine/System/IDGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.System
+{
+    /// <summary>
+    /// Static class which generates new id for world database
+    /// </summary>
+    public static class IDGenerator
+    {
+        static int nextID = 0; // ID
+
+        /// <summary>
+        /// Generate new unique id
+        /// </summary>
+        /// <returns>new unique id</returns>
+        public static int GenerateNewID()
+        {
+            nextID++;
+
+            return nextID;
+        }
+    }
+}
---
+++ Engine/Engine/System/RandomNumberGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace Engine.System
+{
+    /// <summary>
+    /// Random number generator for engine
+    /// </summary>
+    public static class RandomNumberGenerator
+    {
+        private static readonly RNGCryptoServiceProvider generator = new RNGCryptoServiceProvider();
+
+        /// <summary>
+        /// Generate random number
+        /// </summary>
+        /// <param name="min">Minimum value</param>
+        /// <param name="max">Maximum value</param>
+        /// <returns>Number between min and max values</returns>
+        public static int Generate(int min, int max)
+        {
+            byte[] randomNumber = new byte[1];
+
+            generator.GetBytes(randomNumber);
+
+            double asc2ConvertBytes = Convert.ToDouble(randomNumber[0]);
+
+            double multy = Math.Max(0, (asc2ConvertBytes) / 255d);
+            int range = max - min + 1;
+            double randomInRange = Math.Floor(multy * range);
+
+            return (int)(min + randomInRange);
+        }
+    }
+}
