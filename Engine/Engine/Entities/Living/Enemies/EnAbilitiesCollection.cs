5bedb6a0a8911efaa7fc77c4981e649e73b8cb20
Nair Sark <serkorit@gmail.com>
2017-11-17 20:27:29 +0500

    EnAbilities

---
+++ Engine/Engine/Entities/Living/Enemies/EnAbilitiesCollection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public class EnAbilitiesCollection
+    {
+        public EnemyAbility Ability;
+        public int Cooldown;
+
+        public EnAbilitiesCollection(EnemyAbility ability, int cooldown)
+        {
+            Ability = ability;
+            Cooldown = cooldown;
+        }
+    }
+}
--- Engine/Engine/Entities/Living/Enemies/Enemy.cs
+++ Engine/Engine/Entities/Living/Enemies/Enemy.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 
 namespace Engine
 {
+    public delegate void EnemyAbility(params object[] args);
+
     public enum EnemyType
     {
         world,
@@ -18,12 +20,14 @@ namespace Engine
     public class Enemy : Entity
     {
         public List<LootCollection> LootTable { get; set; }
+        public Dictionary<string, EnAbilitiesCollection> Abilities { get; set; }
 
         public Enemy(string name, string desc, int id, int mindmg,
             int maxdmg, int hp, int stamina, int mana, int gold, int exp)
             : base(name, desc, id, mindmg, maxdmg, hp, stamina, mana, gold, exp)
         {
             LootTable = new List<LootCollection>();
+            Abilities = new Dictionary<string, EnAbilitiesCollection>();
         }
 
         public Enemy(Enemy enemy)
